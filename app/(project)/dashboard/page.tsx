d7d7c6e020bb865e7924a24750f8094af5e436cf
Bruno Gomes Ferreira <bruno.gs.ferreira@bradesco.com.br>
2025-04-17 21:23:38 -0300

    feat: Configurando Oauth com google e banco de dados com firebase

---
+++ app/(project)/dashboard/page.tsx
@@ -0,0 +1,32 @@
+import { handleAuth } from "@/app/actions/handle-auth";
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+      <p>
+        {session?.user?.email
+          ? session?.user?.email
+          : "usuario nao esta logado!!"}
+      </p>
+      {session?.user?.email && (
+        <form action={handleAuth}>
+          <button
+            type="submit"
+            className="border rounded-md px-2 py-1 cursor-pointer"
+          >
+            Logout
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
---
+++ app/(project)/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Landing page</h1>
+      <Link href="/login">
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+          Go to Login
+        </button>
+      </Link>
+    </div>
+  );
+}
