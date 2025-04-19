9243e32358a1b02ae821c5dff512bb68bda2f7ef
Bruno Gomes Ferreira <bruno.gs.ferreira@bradesco.com.br>
2025-04-19 17:22:06 -0300

    feat: Add seo to page

--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Template SaaS",
+  description: "This is the landing page of the application.",
+};
 
 export default function Home() {
   return (
