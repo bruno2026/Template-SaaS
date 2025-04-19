7302bb5f5d3ccbef8612bd9d4caca22fc80dcf5a
Bruno Gomes Ferreira <bruno.gs.ferreira@bradesco.com.br>
2025-04-19 16:44:08 -0300

    feat: config SEO

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,12 +1,24 @@
-import { handleAuth } from "@/app/actions/handle-auth";
-export default function Login() {
+import { handleGoogleSignIn } from "@/app/actions/handle-google-auth";
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Login() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Login</h1>
-      <form 
-      action={handleAuth}
-      >
-        <button type="submit">Signin with Google</button>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Login</h1>
+      <form className="mt-10" action={handleGoogleSignIn}>
+        <button
+          className="border rounded-md p-2 cursor-pointer hover:bg-yellow-100"
+          type="submit"
+        >
+          Signin with Google
+        </button>
       </form>
     </div>
   );
